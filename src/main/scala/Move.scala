31735452a2750b9114a142e46ff5f976e91e70c0
Dave <dave@d53dev.net>
2017-03-02 11:17:15 +0100

    Added apply methods for the case classes Forward and Backward

--- src/main/scala/Move.scala
+++ src/main/scala/Move.scala
@@ -8,3 +8,7 @@ abstract class Move(val orientation: Orientation)
 case class MoveForward(override val orientation: Orientation) extends Move(orientation)
 case class MoveBackward(override val orientation: Orientation) extends Move(orientation)
 
+object Move {
+  def apply[MoveForward](orientation: Orientation): MoveForward = new MoveForward(orientation)
+  def apply[MoveBackward](orientation: Orientation): MoveBackward = new MoveBackward(orientation)
+}
