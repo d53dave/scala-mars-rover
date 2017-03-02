1bb1ef56d6cafc2490003b98cc658589b1c34f34
Dave <dave@d53dev.net>
2017-03-02 11:29:45 +0100

    Reverted the apply methods on the companion object of the abstract base class: doesn't work that way.

--- src/main/scala/Move.scala
+++ src/main/scala/Move.scala
@@ -7,8 +7,3 @@ abstract class Move(val orientation: Orientation)
 
 case class MoveForward(override val orientation: Orientation) extends Move(orientation)
 case class MoveBackward(override val orientation: Orientation) extends Move(orientation)
-
-object Move {
-  def apply[MoveForward](orientation: Orientation): MoveForward = new MoveForward(orientation)
-  def apply[MoveBackward](orientation: Orientation): MoveBackward = new MoveBackward(orientation)
-}
