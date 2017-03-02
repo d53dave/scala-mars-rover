69f0914b1e0c0cb05ddd779131557cdc9968885b
Dave <dave@d53dev.net>
2017-03-02 02:02:20 +0100

    Added test case that tests if terrain is at least 1x1 in size

--- src/test/scala/TerrainSpec.scala
+++ src/test/scala/TerrainSpec.scala
@@ -4,6 +4,8 @@ import org.specs2.Specification
   * Created by dsere on 02/03/2017.
   */
 class TerrainSpec extends Specification { def is = s2"""
+  Basic assumptions
+  |   The terrain must be 1x1 or larger $boundsCheck
 
   The terrain should correctly handle moves
   |   moves forward facing north        $forwardNorth
@@ -15,6 +17,8 @@ class TerrainSpec extends Specification { def is = s2"""
   |   moves forward facing west         $forwardWest
   |   moves backward facing west        $backwardWest
                                  """
+
+  def boundsCheck = ???
 
   def forwardNorth = ???
   def backwardNorth = ???
