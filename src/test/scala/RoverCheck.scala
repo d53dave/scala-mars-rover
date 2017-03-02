671623c08dee88b521bc067806692eaf55d89ba0
Dave <dave@d53dev.net>
2017-03-02 12:38:35 +0100

    Fixed error in test case generation and added missing 0 bounds check. Lowered terrain size to trigger more edge cases

--- src/test/scala/RoverCheck.scala
+++ src/test/scala/RoverCheck.scala
@@ -4,14 +4,12 @@ import org.scalacheck.Gen.{choose, listOf}
 import org.scalacheck.{Gen, Prop}
 import org.specs2.{ScalaCheck, Specification}
 
-/**
-  * Created by dsere on 28/02/2017.
-  */
 
 
-class RoverCheck extends Specification with ScalaCheck { def is =
-  s2"""
+class RoverCheck extends Specification with ScalaCheck { def is = s2"""
+
        The Rover should, after arbitrary moves, still be on a valid position on the terrain $t1
+
   """
 
   def t1 = Prop.forAll(genInitialState) { case (width, height, x, y, orientation) =>
@@ -19,19 +17,24 @@ class RoverCheck extends Specification with ScalaCheck { def is =
       commands =>
         val terrain = new Terrain2D(width, height)
         val startPos =  new Position2D(x, y)
+
+        //
         val roverAtLastPos = commands.foldLeft(Rover(terrain, startPos, orientation))(
           (rover, cmd) => rover.move(cmd)
         )
 
         val lastPos = roverAtLastPos.position.asInstanceOf[Position2D]
 
-        (lastPos.x must beLessThan(width)) && (lastPos.y must beLessThan(height))
+        lastPos.x must beLessThan(width)
+        lastPos.x must beGreaterThanOrEqualTo(0L)
+        lastPos.y must beLessThan(height)
+        lastPos.y must beGreaterThanOrEqualTo(0L)
     }
   }
 
   val genInitialState: Gen[(Long, Long, Long, Long, Orientation)] = for {
-    width  <- choose(0, 1000)
-    height <- choose(0, 1000)
+    width  <- choose(1, 100)
+    height <- choose(1, 100)
     startX <- choose(0, width - 1)
     startY <- choose(0, height - 1)
     startOrientation <- Gen.oneOf(North, East, South, West)
