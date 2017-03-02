89780bd5c0ba0f9e3d0e21571abe605cbb54f610
Dave <dave@d53dev.net>
2017-03-02 13:24:51 +0100

    Added basic test cases for Rover

--- src/test/scala/RoverSpec.scala
+++ src/test/scala/RoverSpec.scala
@@ -1,11 +1,22 @@
 package net.d53dev.scalamarsrover
 
-import org.specs2._
+import org.specs2.Specification
+import org.specs2.mock.Mockito
 
-class QuickStartSpec extends Specification { def is = s2"""
 
- This is my first specification
-   it is working                 $ok
-   really working!               $ok
+class RoverSpec extends Specification with Mockito { def is = s2"""
+
+  The rover should correctly rotate
+  |  single rotations               $singleRotation
+  |  multiple rotations             $multiRotation
+
+  The rover should pass the correct move objects to the terrain
+  | when moving forward             $forwardCheck
+  | when moving backward            $backwardCheck
                                  """
+
+  def singleRotation = ???
+  def multiRotation = ???
+  def forwardCheck = ???
+  def backwardCheck = ???
 }
