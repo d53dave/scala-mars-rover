5b169a766fb8371a4440e0a5540dbdfb996175be
Dave <dave@d53dev.net>
2017-03-02 14:25:03 +0100

    Added tests for moving over terrain boundaries, removed unneeded `new` keywords

--- src/test/scala/TerrainSpec.scala
+++ src/test/scala/TerrainSpec.scala
@@ -16,6 +16,12 @@ class TerrainSpec extends Specification { def is = s2"""
   |   moves backward facing south       $backwardSouth
   |   moves forward facing west         $forwardWest
   |   moves backward facing west        $backwardWest
+
+  |  The terrain should correclty handle moves over boundaries
+  |   moves over top boundary           $overTheTop
+  |   moves over left boundary          $overTheLeft
+  |   moves over bottom boundary        $overTheBottom
+  |   moves over right boundary         $overTheRight
                                  """
 
   def boundsCheck = {
@@ -37,7 +43,7 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = North
 
-    val newPos = terrain.performMove(initialPos, new MoveForward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
     newPos.x mustEqual 4
     newPos.y mustEqual 3
   }
@@ -47,7 +53,7 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = North
 
-    val newPos = terrain.performMove(initialPos, new MoveBackward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveBackward(orientation))
     newPos.x mustEqual 4
     newPos.y mustEqual 5
   }
@@ -57,7 +63,7 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = East
 
-    val newPos = terrain.performMove(initialPos, new MoveForward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
     newPos.x mustEqual 3
     newPos.y mustEqual 4
   }
@@ -67,7 +73,7 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = East
 
-    val newPos = terrain.performMove(initialPos, new MoveBackward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveBackward(orientation))
     newPos.x mustEqual 5
     newPos.y mustEqual 4
   }
@@ -77,7 +83,7 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = South
 
-    val newPos = terrain.performMove(initialPos, new MoveForward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
     newPos.x mustEqual 4
     newPos.y mustEqual 5
   }
@@ -87,7 +93,7 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = South
 
-    val newPos = terrain.performMove(initialPos, new MoveBackward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveBackward(orientation))
     newPos.x mustEqual 4
     newPos.y mustEqual 3
   }
@@ -97,7 +103,7 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = West
 
-    val newPos = terrain.performMove(initialPos, new MoveForward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
     newPos.x mustEqual 3
     newPos.y mustEqual 4
   }
@@ -107,8 +113,48 @@ class TerrainSpec extends Specification { def is = s2"""
     val initialPos = Position2D(4, 4)
     val orientation = West
 
-    val newPos = terrain.performMove(initialPos, new MoveBackward(orientation))
+    val newPos = terrain.performMove(initialPos, MoveBackward(orientation))
     newPos.x mustEqual 5
+    newPos.y mustEqual 4
+  }
+
+  def overTheTop = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(4, 0)
+    val orientation = North
+
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
+    newPos.x mustEqual 4
+    newPos.y mustEqual 9
+  }
+
+  def overTheBottom = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(4, 9)
+    val orientation = South
+
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
+    newPos.x mustEqual 4
+    newPos.y mustEqual 0
+  }
+
+  def overTheRight = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(9, 4)
+    val orientation = East
+
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
+    newPos.x mustEqual 0
+    newPos.y mustEqual 4
+  }
+
+  def overTheLeft = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(0, 4)
+    val orientation = West
+
+    val newPos = terrain.performMove(initialPos, MoveForward(orientation))
+    newPos.x mustEqual 9
     newPos.y mustEqual 4
   }
 }
