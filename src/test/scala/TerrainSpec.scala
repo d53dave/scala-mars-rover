f8af676aaabfc3628fddfac81d129f018ea39960
Dave <dave@d53dev.net>
2017-03-02 11:13:43 +0100

    Added some basic test cases for Terrain

--- src/test/scala/TerrainSpec.scala
+++ src/test/scala/TerrainSpec.scala
@@ -1,8 +1,7 @@
+package net.d53dev.scalamarsrover
+
 import org.specs2.Specification
 
-/**
-  * Created by dsere on 02/03/2017.
-  */
 class TerrainSpec extends Specification { def is = s2"""
   Basic assumptions
   |   The terrain must be 1x1 or larger $boundsCheck
@@ -18,10 +17,39 @@ class TerrainSpec extends Specification { def is = s2"""
   |   moves backward facing west        $backwardWest
                                  """
 
-  def boundsCheck = ???
+  def boundsCheck = {
+    def sadCreate: Terrain2D = {
+      Terrain2D(0, 0)
+    }
 
-  def forwardNorth = ???
-  def backwardNorth = ???
+    sadCreate must throwA[Exception]
+
+    def sadCreate2: Terrain2D = {
+      Terrain2D(-10, -9)
+    }
+
+    sadCreate2 must throwA[Exception]
+  }
+
+  def forwardNorth = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(4, 4)
+    val orientation = North
+
+    val newPos = terrain.performMove(initialPos, new MoveForward(orientation))
+    newPos.x mustEqual 4
+    newPos.y mustEqual 3
+  }
+
+  def backwardNorth = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(4, 4)
+    val orientation = North
+
+    val newPos = terrain.performMove(initialPos, new MoveBackward(orientation))
+    newPos.x mustEqual 4
+    newPos.y mustEqual 5
+  }
   def forwardEast = ???
   def backwardEast = ???
   def forwardSouth = ???
