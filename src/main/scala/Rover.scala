1ea4a6c029070a8359028076c615907be57381da
Dave <dave@d53dev.net>
2017-03-02 01:36:35 +0100

    Added apply method in companion object, made Terrain generic

--- src/main/scala/Rover.scala
+++ src/main/scala/Rover.scala
@@ -1,9 +1,14 @@
+package net.d53dev.scalamarsrover
 /**
   * Created by dsere on 28/02/2017.
   */
-class Rover(terrain: Terrain, position: Position) {
+class Rover(val terrain: Terrain[_], val position: Position) {
   def move(command: RoverCommand): Rover = {
     ???
   }
+}
+
+object Rover {
+  def apply(terrain: Terrain[_], position: Position) = new Rover(terrain, position)
 }
 
