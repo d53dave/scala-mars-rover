07e42b72fd899ffd16d9354b6bcbeec976a7fff4
Dave <dave@d53dev.net>
2017-03-02 01:39:11 +0100

    Rover should also have orientation

--- src/main/scala/Rover.scala
+++ src/main/scala/Rover.scala
@@ -2,13 +2,14 @@ package net.d53dev.scalamarsrover
 /**
   * Created by dsere on 28/02/2017.
   */
-class Rover(val terrain: Terrain[_], val position: Position) {
+class Rover(val terrain: Terrain[_], val position: Position, val orientation: Orientation) {
   def move(command: RoverCommand): Rover = {
     ???
   }
 }
 
 object Rover {
-  def apply(terrain: Terrain[_], position: Position) = new Rover(terrain, position)
+  def apply(terrain: Terrain[_], position: Position, orientation: Orientation) =
+    new Rover(terrain, position, orientation)
 }
 
