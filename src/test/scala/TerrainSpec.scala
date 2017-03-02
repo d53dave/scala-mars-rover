3cea63189752239bf774aa95c879f0b1117127fd
Dave <dave@d53dev.net>
2017-03-02 02:00:44 +0100

    Added base test cases for terrain

--- src/test/scala/TerrainSpec.scala
+++ src/test/scala/TerrainSpec.scala
@@ -5,8 +5,23 @@ import org.specs2.Specification
   */
 class TerrainSpec extends Specification { def is = s2"""
 
- This is my first specification
-   it is working                 $ok
-   really working!               $ok
+  The terrain should correctly handle moves
+  |   moves forward facing north        $forwardNorth
+  |   moves backward facing north       $backwardNorth
+  |   moves forward facing east         $forwardEast
+  |   moves backward facing east        $backwardEast
+  |   moves forward facing south        $forwardSouth
+  |   moves backward facing south       $backwardSouth
+  |   moves forward facing west         $forwardWest
+  |   moves backward facing west        $backwardWest
                                  """
+
+  def forwardNorth = ???
+  def backwardNorth = ???
+  def forwardEast = ???
+  def backwardEast = ???
+  def forwardSouth = ???
+  def backwardSouth = ???
+  def forwardWest = ???
+  def backwardWest = ???
 }
