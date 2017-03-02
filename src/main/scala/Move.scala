68c880634c226810e9e08ddda0eae00af864cc20
Dave <dave@d53dev.net>
2017-03-02 02:11:02 +0100

    orientation needs to be 'val' to be accessible

--- src/main/scala/Move.scala
+++ src/main/scala/Move.scala
@@ -2,9 +2,9 @@ package net.d53dev.scalamarsrover
 /**
   * Created by dsere on 28/02/2017.
   */
-abstract class Move(orientation: Orientation)
+abstract class Move(val orientation: Orientation)
 
 
-case class MoveForward(orientation: Orientation) extends Move(orientation)
-case class MoveBackward(orientation: Orientation) extends Move(orientation)
+case class MoveForward(override val orientation: Orientation) extends Move(orientation)
+case class MoveBackward(override val orientation: Orientation) extends Move(orientation)
 
