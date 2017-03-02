6c0d0c8b465aee19e80537e8d62050b88b177bec
Dave <dave@d53dev.net>
2017-03-02 13:36:53 +0100

    Added implementation for rotation test cases

--- src/test/scala/RoverSpec.scala
+++ src/test/scala/RoverSpec.scala
@@ -2,6 +2,7 @@ package net.d53dev.scalamarsrover
 
 import org.specs2.Specification
 import org.specs2.mock.Mockito
+import spatial._
 
 
 class RoverSpec extends Specification with Mockito { def is = s2"""
@@ -15,8 +16,44 @@ class RoverSpec extends Specification with Mockito { def is = s2"""
   | when moving backward            $backwardCheck
                                  """
 
-  def singleRotation = ???
-  def multiRotation = ???
+  def singleRotation = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(4, 4)
+
+    var rover = Rover(terrain, initialPos, North)
+
+    rover.move(Left).orientation mustEqual West
+    rover.move(Right).orientation mustEqual East
+
+    rover = Rover(terrain, initialPos, East)
+    rover.move(Left).orientation mustEqual North
+    rover.move(Right).orientation mustEqual South
+
+    rover = Rover(terrain, initialPos, South)
+    rover.move(Left).orientation mustEqual East
+    rover.move(Right).orientation mustEqual West
+
+    rover = Rover(terrain, initialPos, West)
+    rover.move(Left).orientation mustEqual South
+    rover.move(Right).orientation mustEqual North
+
+  }
+  def multiRotation = {
+    val terrain = Terrain2D(10, 10)
+    val initialPos = Position2D(4, 4)
+    val initialOrientation = North
+
+    val rover = Rover(terrain, initialPos, initialOrientation)
+
+    rover.move(Left).move(Left).orientation mustEqual South
+    rover.move(Left).move(Left).move(Left).orientation mustEqual East
+    rover.move(Left).move(Left).move(Left).move(Left).orientation mustEqual North
+
+    rover.move(Right).move(Right).orientation mustEqual South
+    rover.move(Right).move(Right).move(Right).orientation mustEqual West
+    rover.move(Right).move(Right).move(Right).move(Right).orientation mustEqual North
+  }
+
   def forwardCheck = ???
   def backwardCheck = ???
 }
