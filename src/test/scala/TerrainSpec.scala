98971c07f2ca678c221d7f3d2625cc5969edb1c2
Dave <dave@d53dev.net>
2017-03-02 01:38:14 +0100

    Added empty spec for Terrain

---
+++ src/test/scala/TerrainSpec.scala
@@ -0,0 +1,12 @@
+import org.specs2.Specification
+
+/**
+  * Created by dsere on 02/03/2017.
+  */
+class TerrainSpec extends Specification { def is = s2"""
+
+ This is my first specification
+   it is working                 $ok
+   really working!               $ok
+                                 """
+}
